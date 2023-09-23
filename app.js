f2f095349b6adf3cf7a412fe5ab0c5f0c267a45a
tanuj <tanujmaurya40@gmail.com>
2023-09-23 18:28:49 +0530

    Added Cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const { default: mongoose } = require("mongoose");
 const app = express();
+const cors= require("cors");
 require("dotenv").config()
 const port = process.env.port || 5000;
 app.use(express.json())
+app.use(cors());
 
 mongoose.connect(process.env.dbLink)
 
